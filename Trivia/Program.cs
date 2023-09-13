e09590a347348b5463a5d0a7d792fba0d0c32b88
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2023-09-13 10:13:10 +0200

    commit

--- Trivia/Program.cs
+++ Trivia/Program.cs
@@ -1,8 +1,6 @@
 ﻿int points = 0;
 Console.WriteLine("Welcome to the Leo Trivia!");
 
-// -----------------------------------------------------------------------------
-
 Console.WriteLine("Allllriighhhttt! Let's get this trivia rolling!");
 Console.WriteLine("Quueesstiiooon Nummberr Oooonee!");
 Console.WriteLine("Which fictional character is objectionally the hottest?");
@@ -18,11 +16,9 @@ else
   Console.WriteLine("What the fuck? No. Why did you think that would even be remotely close to the correct answer?");
 }
 
-// -----------------------------------------------------------------------------
-
 Console.WriteLine("Movin onto Nuuummbbeeerrr Ttwooooo!");
-Console.WriteLine("");
-Console.WriteLine("a) Galactus  b) Unicorn  c) Unicron");
+Console.WriteLine("How many hours have I spent in your walls this week?");
+Console.WriteLine("a) 32 hours  b) 40 hours  c) I never left");
 string answer2 = Console.ReadLine();
 if (answer2 == "c")
 {
@@ -31,39 +27,37 @@ if (answer2 == "c")
 }
 else
 {
-  Console.WriteLine("INCORRECT");
+  Console.WriteLine("HAHAHAHAHAH, did you really think I'd leave a place as comfortable as your walls, the view is too intoxicating.");
 }
 
-// -----------------------------------------------------------------------------
-
-Console.WriteLine("3: Vad heter Scorponoks headmaster?");
-Console.WriteLine("a) Lord Zarak  b) Lord Hi-Q  c) Lord Razorfist");
+Console.WriteLine("And last, BUT NOT LEAST! Nummmbbeerrrrrr Thhrrreeeeeee!");
+Console.WriteLine("Does your skin feel good on you?");
+Console.WriteLine("a) No b) NO c) OF COURSE NOT, I WANT TO RIP IT OFF");
 string answer3 = Console.ReadLine();
-if (answer3 == "a")
+if (answer3 == "c")
 {
   points++;
-  Console.WriteLine("CORRECT");
+  Console.WriteLine("HAHAHAHAHAH WOW, YOU'RE TOO GOOD AT THIS!");
 }
 else
 {
-  Console.WriteLine("INCORRECT");
+  Console.WriteLine("I NEED MORE PASSION!");
 }
 
-// -----------------------------------------------------------------------------
-
-Console.WriteLine($"Du fick {points} poäng.");
+Console.WriteLine("ANNNDDD WE'RE DONE.");
+Console.WriteLine($"You got a total of {points} points");
 if (points == 0)
 {
-  Console.WriteLine("Du har alltså ingen koll alls.");
+  Console.WriteLine("This is fucking horrible, you serve zero purpose.");
 }
 else if (points < 3)
 {
-  Console.WriteLine("OK, du har hyfsad koll - eller gissade rätt en eller ett par gånger.");
+  Console.WriteLine("Not the worst, but I WANT BETTER");
 }
 else 
 {
-  Console.WriteLine("Grattis! Du hade alla rätt!");
+  Console.WriteLine("CONGRATULATIONS, YOU'VE WON!");
 }
 
-Console.WriteLine("Tryck på valfri tangent för att avsluta.");
+Console.WriteLine("Click any button to end this shitfest.");
 Console.ReadKey();
