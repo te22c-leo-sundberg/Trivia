b48a7aee00961933344913649925e6344bafbeec
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2023-09-13 10:45:21 +0200

    adad

--- Trivia/Program.cs
+++ Trivia/Program.cs
@@ -1,4 +1,6 @@
-﻿int points = 0;
+﻿while(true)
+{
+int points = 0;
 Console.WriteLine("Welcome to the Leo Trivia!");
 
 Console.WriteLine("Allllriighhhttt! Let's get this trivia rolling!");
@@ -8,12 +10,12 @@ Console.WriteLine("a) Bridget  b) Astolfo  c) Felix");
 string answer1 = Console.ReadLine().ToLower();
 if (answer1 == "a")
 {   
-  points++;
-  Console.WriteLine("Wow! You're a natural. Keep it up like this and we could be friends!");
+    points++;
+    Console.WriteLine("Wow! You're a natural. Keep it up like this and we could be friends!");
 }
 else
 {
-  Console.WriteLine("What the fuck? No. Why did you think that would even be remotely close to the correct answer?");
+    Console.WriteLine("What the fuck? No. Why did you think that would even be remotely close to the correct answer?");
 }
 
 Console.WriteLine("Movin onto Nuuummbbeeerrr Ttwooooo!");
@@ -22,12 +24,12 @@ Console.WriteLine("a) 32 hours  b) 40 hours  c) I never left");
 string answer2 = Console.ReadLine();
 if (answer2 == "c")
 {
-  points++;
-  Console.WriteLine("WOW!!! You're practically a genius with the way you're blowing through these questions left and right!");
+    points++;
+    Console.WriteLine("WOW!!! You're practically a genius with the way you're blowing through these questions left and right!");
 }
 else
 {
-  Console.WriteLine("HAHAHAHAHAH, did you really think I'd leave a place as comfortable as your walls, the view is too intoxicating.");
+    Console.WriteLine("HAHAHAHAHAH, did you really think I'd leave a place as comfortable as your walls, the view is too intoxicating.");
 }
 
 Console.WriteLine("And last, BUT NOT LEAST! Nummmbbeerrrrrr Thhrrreeeeeee!");
@@ -36,28 +38,40 @@ Console.WriteLine("a) No b) NO c) OF COURSE NOT, I WANT TO RIP IT OFF");
 string answer3 = Console.ReadLine();
 if (answer3 == "c")
 {
-  points++;
-  Console.WriteLine("HAHAHAHAHAH WOW, YOU'RE TOO GOOD AT THIS!");
+    points++;
+    Console.WriteLine("HAHAHAHAHAH WOW, YOU'RE TOO GOOD AT THIS!");
 }
 else
 {
-  Console.WriteLine("I NEED MORE PASSION!");
+    Console.WriteLine("I NEED MORE PASSION!");
 }
 
 Console.WriteLine("ANNNDDD WE'RE DONE.");
 Console.WriteLine($"You got a total of {points} points");
 if (points == 0)
 {
-  Console.WriteLine("This is fucking horrible, you serve zero purpose.");
+    Console.WriteLine("This is fucking horrible, you serve zero purpose.");
+}
+else if (points < 2)
+{
+    Console.WriteLine("Not the worst, but I WANT BETTER");
 }
 else if (points < 3)
 {
-  Console.WriteLine("Not the worst, but I WANT BETTER");
+    Console.WriteLine("Close, but NOT CLOSE ENOUGH!")
 }
 else 
 {
-  Console.WriteLine("CONGRATULATIONS, YOU'VE WON!");
+    Console.WriteLine("CONGRATULATIONS, YOU'VE WON!");
 }
 
-Console.WriteLine("Click any button to end this shitfest.");
-Console.ReadKey();
+while(true)
+{
+    Console.WriteLine("Would you like to try again? [Y/N]");
+    string answer = Console.ReadLine().ToUpper();
+    if (answer == "Y")
+    break;
+    if (answer == "N")
+        return;
+}
+}
